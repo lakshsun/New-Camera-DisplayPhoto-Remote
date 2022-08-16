afe7efb6719096f5e7d72d97031da796bef5e8de
Lakshmi Sunder <lakshmisunder2005@gmail.com>
2022-08-16 12:38:16 -0500

    Initial Commit

---
+++ Camera&DisplayPhoto/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Camera&DisplayPhoto
+//
+//  Created by Lakshmi Sunder on 8/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var imagePicker = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func choosePhotoTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func takeSelfieTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBOutlet weak var displayImage: UIImageView!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info [UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                displayImage.image = selectedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+}
+
